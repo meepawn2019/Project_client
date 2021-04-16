03e5ba87a371332c3a844e3aa3a0d658bc5367fa
meepawn2019 <meepawn2018@gmail.com>
2021-04-16 15:20:25 +0700

    change css

--- src/components/Header/NavigateButtons/NavigateButton.js
+++ src/components/Header/NavigateButtons/NavigateButton.js
@@ -1,6 +1,36 @@
-import React from "react";
-import styles from "./NavigateButton.module.css";
+import React, { useState, useRef } from "react";
 import { Dropdown } from "react-bootstrap";
+import { makeStyles } from "@material-ui/core/styles";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+
+const useStyles = makeStyles({
+  buttonMain: {
+    backgroundColor: "#f2ba49",
+    border: "1px solid #efefef",
+    color: "#fff",
+    width: "160px",
+    "&:hover": {
+      color: "#fff",
+      backgroundColor: "#992420",
+    },
+  },
+  buttonLight: {
+    background: "#fff",
+    border: "1px solid #f2ba49",
+    color: "#f2ba49",
+    "&:hover": {
+      background: "#f2f2f2",
+    },
+  },
+});
 
 export default function NavigateButton(props) {
   const {
@@ -9,41 +39,87 @@ export default function NavigateButton(props) {
     onShowRegisterModal,
     onShowArticleModal,
   } = props;
+  const classes = useStyles();
+
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
   return (
     <div className="my-2 my-lg-0 d-flex align-items-center justify-content-between">
       <div className="d-flex">
         <button
-          className={`buttonMain ${styles["button--main"]} mr-2`}
+          className={`buttonMain ${classes.buttonMain} mr-2`}
           onClick={onShowPostQuestionModal}
         >
           Đặt câu hỏi
         </button>
         <button
-          className={`buttonMain ${styles["button--light"]} mr-3`}
+          className={`buttonMain ${classes.buttonLight} mr-3`}
           onClick={onShowArticleModal}
         >
           Viết bài
         </button>
       </div>
       <div className="align-items-center d-flex ml-3">
-        <Dropdown>
-          <Dropdown.Toggle
-            id="dropdown-basic"
-            style={{
-              backgroundColor: "gray",
-              borderRadius: "50%",
-              border: "none",
-            }}
-          ></Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item as="button" onClick={onShowLoginModal}>
-              Đăng nhập
-            </Dropdown.Item>
-            <Dropdown.Item as="button" onClick={onShowRegisterModal}>
-              Đăng ký
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+        <IconButton
+          ref={anchorRef}
+          aria-controls={open ? "menu-list-grow" : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+          className={classes.margin}
+          size="small"
+        >
+          <ArrowDownwardIcon fontSize="inherit" />
+        </IconButton>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom" ? "center top" : "center bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="menu-list-grow"
+                    onKeyDown={handleListKeyDown}
+                  >
+                    <MenuItem onClick={onShowLoginModal}>Login</MenuItem>
+                    <MenuItem onClick={onShowRegisterModal}>Register</MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
       </div>
     </div>
   );

686ce146a3005b56b30876d4b83c79be540f6c12
meepawn2019 <meepawn2018@gmail.com>
2021-04-17 01:29:01 +0700

    add forgot password

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Profile from "./pages/Profile/Profile";
 import PrimarySearchAppBar from "./components/NavBar/NavBar";
 import Setting from "./pages/Setting";
-import Question from './pages/Question/Question'
-import AuthContext from './appContext';
+import Question from "./pages/Question/Question";
+import AuthContext from "./appContext";
+import ForgotPasswords from "./pages/ForgotPassword/ForgotPassword";
 
 function App() {
   return (
     // <AuthContext.Provider>
-      <Router>
-        <PrimarySearchAppBar />
+    <Router>
+      <PrimarySearchAppBar />
 
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route path="/profile" component={Profile}></Route>
-          <Route path="/setting" component={Setting}></Route>
-          <Route path="/question/:id" component={Question}></Route>
-
-        </Switch>
-      </Router>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/profile" component={Profile}></Route>
+        <Route path="/setting" component={Setting}></Route>
+        <Route path="/question/:id" component={Question}></Route>
+        <Route path="/forgot" component={ForgotPasswords}></Route>
+      </Switch>
+    </Router>
     // </AuthContext.Provider>
   );
 }
---
+++ src/components/AnswerCard.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Editor from "./Editor/Editor";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import { Button } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  container: {
+    boxShadow: "0 2px 15px rgb(0 0 0 / 22%)",
+    padding: "20px",
+    backgroundColor: "#fff",
+    width: "1100px",
+  },
+  buttonMain: {
+    marginTop: "10px",
+    float: "right",
+  },
+});
+
+export default function AnswerCard() {
+  const [detailAnswer, setDetailAnswer] = useState();
+  function handleDetailAnswer(v) {
+    setDetailAnswer(v);
+  }
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <Editor value={detailAnswer || ""} onChange={handleDetailAnswer} />
+      <Button
+        className={classes.buttonMain}
+        variant="contained"
+        color="primary"
+      >
+        Đăng
+      </Button>
+    </div>
+  );
+}
